475f1d2f33ec4d32d2943fd2db37b1092797391f
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-12-06 07:16:13 +0000

    one more

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,5 +35,4 @@ min_val = st.number_input('最小値を入力してください', value=0)
 max_val = st.number_input('最大値を入力してください', value=10)
 
 if st.button('乱数を生成'):
-    random_num = random.randint(min_val, max_val)
-    st.write(f'生成された乱数: {random_num}')
+    st.write(f'生成された乱数: {random.randint(min_val, max_val)}')
