124ff27ef0896a87fa66f0cc1602f3ee3f38543d
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-12-12 06:55:05 +0000

    重複

--- pages/sekigae.py
+++ pages/sekigae.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # 8x7のランダムな数値を持つDataFrameを作成する
-data = np.random.randint(1, 42, size=(6, 7))
+data = np.random.choice(np.arange(1, 42), size=(6, 7), replace=False)
 df = pd.DataFrame(data, columns=[f"Column {i+1}" for i in range(7)])
 
 # Streamlitアプリケーションの作成
