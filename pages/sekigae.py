707e29f36967105c6fb58f7c274f6b7dbd9ab2ce
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-12-12 06:52:59 +0000

    random

--- pages/sekigae.py
+++ pages/sekigae.py
@@ -8,4 +8,11 @@ df = pd.DataFrame(data, columns=[f"Column {i+1}" for i in range(7)])
 
 # Streamlitアプリケーションの作成
 st.write("8x7 Table with Random Values")
-df
+
+# ボタンを追加して数値をシャッフルして表示する
+if st.button("シャッフル"):
+    # データフレームの値をシャッフルする
+    shuffled_df = df.apply(np.random.permutation, axis=0)
+    shuffled_df
+else:
+    df
