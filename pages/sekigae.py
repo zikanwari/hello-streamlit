483169474edd51de20f66d1c9cab24639d69d0df
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-12-12 07:06:36 +0000

    aaa

--- pages/sekigae.py
+++ pages/sekigae.py
@@ -9,10 +9,4 @@ df = pd.DataFrame(data, columns=[f"Column {i+1}" for i in range(7)])
 # Streamlitアプリケーションの作成
 st.write("席替え")
 
-# ボタンを追加して数値をシャッフルして表示する
-if st.button("シャッフル"):
-    new_data = np.random.choice(np.setdiff1d(np.arange(1, 42), df.values.flatten()), size=(6, 7), replace=False)
-    df = pd.DataFrame(new_data, columns=[f"Column {i+1}" for i in range(7)])
-    df
-else:
     df
