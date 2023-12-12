5d4c1d79b261a91a80304d494cd264c02284e77b
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-12-12 06:58:47 +0000

    aaa

--- pages/sekigae.py
+++ pages/sekigae.py
@@ -2,17 +2,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# 8x7のランダムな数値を持つDataFrameを作成する
-data = np.random.choice(np.arange(1, 42), size=(6, 7), replace=False)
+# 重複のない8x7のランダムな数値を持つDataFrameを作成する
+data = np.random.choice(np.arange(1, 101), size=(8, 7), replace=False)
 df = pd.DataFrame(data, columns=[f"Column {i+1}" for i in range(7)])
 
 # Streamlitアプリケーションの作成
-st.write("席替え")
+st.write("8x7 Table with Unique Random Values")
 
 # ボタンを追加して数値をシャッフルして表示する
 if st.button("シャッフル"):
-    new_data = np.random.choice(np.setdiff1d(np.arange(1, 42), df.values.flatten()), size=(6, 7), replace=False)
+    # 重複のない新しいデータを作成し、データフレームを更新する
+    new_data = np.random.choice(np.setdiff1d(np.arange(1, 101), df.values.flatten()), size=(8, 7), replace=False)
     df = pd.DataFrame(new_data, columns=[f"Column {i+1}" for i in range(7)])
-    df
+    st.write(df)
 else:
-    df
+    st.write(df)
