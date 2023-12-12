19a41fabe829604fee379a6e21c9118f5810a478
launchpencil <66775280+launchpencil@users.noreply.github.com>
2023-12-12 06:53:46 +0000

    6*7

--- pages/sekigae.py
+++ pages/sekigae.py
@@ -3,11 +3,11 @@ import pandas as pd
 import numpy as np
 
 # 8x7のランダムな数値を持つDataFrameを作成する
-data = np.random.randint(1, 100, size=(8, 7))
+data = np.random.randint(1, 42, size=(6, 7))
 df = pd.DataFrame(data, columns=[f"Column {i+1}" for i in range(7)])
 
 # Streamlitアプリケーションの作成
-st.write("8x7 Table with Random Values")
+st.write("席替え")
 
 # ボタンを追加して数値をシャッフルして表示する
 if st.button("シャッフル"):
